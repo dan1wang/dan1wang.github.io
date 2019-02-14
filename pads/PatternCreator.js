8122de2b1518f3d43c4e5829fb80444ba49b2f61
dan1wang <38196313+dan1wang@users.noreply.github.com>
2019-02-14 16:49:47 +0800

    Update PatternCreator.js

--- pads/PatternCreator.js
+++ pads/PatternCreator.js
@@ -2,63 +2,95 @@
  * @fileoverview Pattern Creator main script
  */
 
+/* exported PatternGeometryType */
+/* exported MaskStateOption, PasteStateOption */
+/* exported PinModification */
+
+/** Enum for package type
+ * @readonly
+ * @enum {number}
+ */
+const PatternGeometryType = {
+  LINE: 1,
+  RECT: 2,
+  ELLIPSE: 3,
+  POLYGON: 9,
+};
+
+/** @typedef {Object} PatternGeometryData
+ * @property {PatternGeometryType} type
+ * @property {Object} data
+ */
+
 /** @typedef {Object} PadStack
- * @property {Array} top
- * @property {Array} topPaste
- * @property {Array} topMask
- * @property {Array} topSilk
- * @property {Array} topAssembly
- * @property {Array} [bottom]
- * @property {Array} [bottomPaste]
- * @property {Array} [bottomMask]
- * @property {Array} [bottomSilk]
- * @property {Array} [bottomAssembly]
+ * @property {Array.<PatternGeometryData>} top
+ * @property {Array.<PatternGeometryData>} topPaste
+ * @property {Array.<PatternGeometryData>} topMask
+ * @property {Array.<PatternGeometryData>} topSilk
+ * @property {Array.<PatternGeometryData>} topAssembly
+ * @property {Array.<PatternGeometryData>} topKeepout
+ * @property {Array.<PatternGeometryData>} bottom
+ * @property {Array.<PatternGeometryData>} bottomPaste
+ * @property {Array.<PatternGeometryData>} bottomMask
+ * @property {Array.<PatternGeometryData>} bottomSilk
+ * @property {Array.<PatternGeometryData>} bottomAssembly
+ * @property {Array.<PatternGeometryData>} bottomKeepout
  */
 
 /** @typedef {Object} PadTemplate
-  * @property {TerminalStyle} padStyle - Pad style (D shape, rectangular, etc.)
-  * @property {PadProperties} props - Pad properties (length, width, etc.)
-  * @property {PadStack} padStack - Calculated pad stack
-
-/** @typedef {Object} Pad
- * @property {number} pinNumber - Pin number
- * @property {number} pinMod - Specify if the pin is hidden or deleted
- * @property {number} positionX - X position
- * @property {number} positionY - Y position
- * @property {number} rotation - Rotation (counter-clockwise) in degree
- * @property {boolean} useDefault - Specify if the pad uses the default style
- * @property {TerminalStyle} padStyle - Pad style (D shape, rectangular, etc.)
- * @property {PadProperties} props - Pad properties (length, width, etc.)
+ * @property {number} uid - Numerically incremented id
+ * @property {string} name - Name of the template
+ * @property {TerminalShape} shape - Base shape (D-shape, rectangular, etc.)
+ * @property {PadProperties} props - Properties (length, width, etc.)
  * @property {PadStack} padStack - Calculated pad stack
  */
 
+/** @typedef {Object} Pad
+ * @property {number} pinNumber - Pin number
+ * @property {PinModification} pinMod - Specify if the pin is hidden or deleted
+ * @property {number} positionX - X position
+ * @property {number} positionY - Y position
+ * @property {number} rotation - Rotation (counter-clockwise) in degree
+ * @property {number} useTemplate - Id of the template used
+ * @property {PadStack} padStack - Computed pad stack
+ */
+
 /** @typedef {Object} Pattern
-  * @property {string} name - Name of the pattern
-  * @property {PackageType} packageType - Package type (QFN, LQFP, etc.)
-  * @property {PatternProperties} props - Properties (Dimension, pitch, etc.)
-  * @property {Array.<Pad>} pads - Pads
-  * @property {Pad} defaultPad - Pad
-  */
+ * @property {string} name - Name of the pattern
+ * @property {string} refDes - RefDes prefix
+ * @property {string} value - Value
+ * @property {string} datasheetUrl - URI of datasheet
+ * @property {string} modelUrl - URI of 3D model
+ * @property {PackageType} packageType - Package type (QFN, LQFP, etc.)
+ * @property {PatternProperties} props - Properties (Dimension, pitch, etc.)
+ * @property {Array.<Pad>} pads - Pads
+ * @property {Array.<PadTemplate>} padTemplates - Pad templates
+ */
 
 /** @typedef {Object} PatternProperties
-  * @property {number} bodyWidth    (E) Vertical length of the component body
-  * @property {number} bodyLength   (D) Horizontal length of the component body
-  * @property {number} [bodyHeight] (?) Height of the component body
-  * @property {number} termWidth    (b) Default width of the terminal/lead
-  * @property {number} termLength   (L) Default length of the terminal/lead
-  * @property {TerminalStyle} termStyle   Terminal style
-  * @property {number} [pitch]      (e) Pad row pitch
-  * @property {number} [pinCount]   (n) Number of pins (excluding thermal pad)
-  * @property {number} JT (JT) Toe
-  * @property {number} JH (JH) Heel
-  * @property {number} JS (JS) Side
-  */
+ * @property {number} bodyWidth    (E) Vertical length of the component body
+ * @property {number} bodyLength   (D) Horizontal length of the component body
+ * @property {number} [bodyHeight] (?) Height of the component body
+ * @property {number} termWidth    (b) Default width of the terminal/lead
+ * @property {number} termLength   (L) Default length of the terminal/lead
+ * @property {TerminalShape} termShape   Terminal style
+ * @property {number} [pitch]      (e) Pad row pitch
+ * @property {number} [pinCount]   (n) Number of pins (excluding thermal pad)
+ * @property {number} JT (JT) Toe
+ * @property {number} JH (JH) Heel
+ * @property {number} JS (JS) Side
+ */
 
 /** @typedef {Object} PadProperties
-  * @property {number} padWidth    (X1)
-  * @property {number} padLength   (Y1)
-  * @property {TerminalStyle} padStyle   Terminal style
-  */
+ * @property {number} padWidth    (X1)
+ * @property {number} padLength   (Y1)
+ * @property {MaskStateOption} topMaskState
+ * @property {MaskStateOption} bottomMaskState
+ * @property {PasteStateOption} topPasteState
+ * @property {PasteStateOption} bottomPasteState
+ * @property {number|string} maskSwell - Mask swell (number or 'auto')
+ * @property {number|string} pasteShrink - Paste shrink (number or 'auto')
+ */
 
 /** Enum for package type
  * @readonly
@@ -69,18 +101,31 @@ const PackageType = {
   LQFP: 'LQFP',
 };
 
-/** Enum for terminal style
+/** Enum for terminal shape
  * @readonly
  * @enum {string}
  */
-const TerminalStyle = {
-  DSHAPE: 'D',
-  RECT: 'R',
+const TerminalShape = {
+  DSHAPE: 'd',
+  RECT: 'r',
+  CIRCULAR: 'c',
+  OBLONG: 'b', // oval
+  IRREGULAR: 'u',
 };
 
-/*  exported PinModification */
-/**
- * Enum for pin modification
+/** Enum for mask state option
+ * @readonly
+ * @enum {number}
+ */
+const MaskStateOption = {COMMON: 1, OPEN: 2, TENTED: 3};
+
+/** Enum for paste state option
+ * @readonly
+ * @enum {number}
+ */
+const PasteStateOption = {COMMON: 1, SOLDER: 2, NO_SOLDER: 3};
+
+/** Enum for pin modification
  * @readonly
  * @enum {number}
  */
@@ -104,7 +149,6 @@ const PinModification = {
   V thermal via diameter
   */
 
-
 /**
  * Pattern creator
  * @constructor
@@ -120,14 +164,18 @@ function PatternCreator() {
   PatternCreator['newQfnPattern'] = function(name) {
     /** @type {Pattern} */
     const newPattern = {
-      'name': (typeof name == 'string')?name:'New Pattern',
+      'name': (typeof name == 'string')?name:'Untitled',
+      'refDes': 'U',
+      'value': '',
+      'datasheetUrl': '',
+      'modelUrl': '',
       'packageType': PackageType.QFN,
       'props': {
         'bodyLength': 5,
         'bodyWidth': 5,
         'termLength': 0.55,
         'termWidth': 0.24,
-        'termStyle': TerminalStyle.DSHAPE,
+        'termShape': TerminalShape.DSHAPE,
         'pitch': 0.5,
         'pinCount': 28,
         'JT': 0.4,
@@ -135,6 +183,7 @@ function PatternCreator() {
         'JS': 0,
       },
       'pads': [],
+      'padTemplates': [],
     };
     return newPattern;
   };
@@ -149,4 +198,4 @@ function PatternCreator() {
   return PatternCreator;
 }
 
-window['PatternCreator'] = PatternCreator;
+window['PatternCreator'] = new PatternCreator();
